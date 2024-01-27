1afa01abaad80912b1f9250de60239ce7619e803
YUKAI <107863135+oniyukai@users.noreply.github.com>
2024-01-27 23:16:37 +0800

    Update v2.2_23.08.05.py

--- python/v2.2_23.08.05.py
+++ python/v2.2_23.08.05.py
@@ -1,18 +1,18 @@
 import random
 
-print('----\nPassword Generator\nVersion 2.2_23.08.05\nhttps://github.com/oniyukai/Password-Generator\n----\n')
-askings = ('Whether to include 0-9? [Y/N] ', 'Whether to include a-z? [Y/N] ', 'Whether to include A-Z? [Y/N] ', 'Whether to include !@#$%^&* ? [Y/N] ')
-include = ('0123456789', 'abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '!@#$%^&*')
+ASKINGS = ('Whether to include 0-9? [Y/N] ', 'Whether to include a-z? [Y/N] ', 'Whether to include A-Z? [Y/N] ', 'Whether to include !@#$%^&* ? [Y/N] ')
+INCLUDE = ('0123456789', 'abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '!@#$%^&*')
+print('----\nPassword Generator\nVersion 2.2_23.08.05\nhttps://github.com/oniyukai/Password-Generator\n----')
 
 while True:
     characters = ''
-    length = input('\nInput the length of password: ')
-    sets = input('Input several sets of generated passwords: ')
+    length = int(input('\nInput the length of password: '))
+    sets = int(input('Input several sets of generated passwords: '))
     for i in range(4):
-        answer = input(askings[i])
-        characters += include[i] if answer=='Y' or answer=='y' else ''
+        answer = input(ASKINGS[i])
+        characters += INCLUDE[i] if answer=='Y' or answer=='y' else ''
     answer = input('Whether to input additions? [Y/N] ')
     characters += input('Input additions: ') if answer=='Y' or answer=='y' else ''
     print('\nGenerated password:')
-    for i in range(int(sets)):
-        print(''.join(random.choices(characters, k=int(length))))
+    for _ in range(sets):
+        print(''.join(random.choices(characters, k=length)))
