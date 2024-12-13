3121998dfa1199b21fbf6278050c5a29e633bc5a
YUKAI <No Email>
2024-12-14 04:04:19 +0800

    更新並優化修改了一些
    更新同檔名同版本簽名但不同內容的檔案

--- length_spinboxs_vcmd test.py
+++ length_spinboxs_vcmd test.py
@@ -1,49 +1,32 @@
 import tkinter as tk
 
-
 #限數字,不能是空,小於2字元
-def vcmd(spinbox_entry): 
-    if str.isdigit(spinbox_entry):
-        if len(spinbox_entry) < 3:
-            return True
-        else:
-            return False
-    else:
-        return False
-lambda spinbox_entry: (True if len(spinbox_entry) < 3 else False) if str.isdigit(spinbox_entry) else False
+def vcmd(spinbox_entry):
+    return str.isdigit(spinbox_entry) and len(spinbox_entry) < 3
+lambda spinbox_entry: str.isdigit(spinbox_entry) and len(spinbox_entry) < 3
 
 #限數字,小於2字元
 def vcmd(spinbox_entry): 
-    if str.isdigit(spinbox_entry):
-        if len(spinbox_entry) < 3:
-                return True
-        else:
-            return False
-    elif spinbox_entry == '':
-        return True
-    else:
-        return False
-lambda spinbox_entry: (True if spinbox_entry=='' else (True if len(spinbox_entry) < 3 else False)) if str.isdigit(spinbox_entry) or spinbox_entry=='' else False
+    return str.isdigit(spinbox_entry) and len(spinbox_entry) < 3 or spinbox_entry == ''
+lambda spinbox_entry: str.isdigit(spinbox_entry) and len(spinbox_entry) < 3 or spinbox_entry == ''
 
 #限數字,不能是空
 str.isdigit
 
 #限數字
-def vcmd(spinbox_entry): 
-    if str.isdigit(spinbox_entry) or spinbox_entry=='':
-        return True
-    else:
-        return False
-lambda spinbox_entry: True if str.isdigit(spinbox_entry) or spinbox_entry=='' else False
+def vcmd(spinbox_entry):
+    return str.isdigit(spinbox_entry) or spinbox_entry==''
+lambda spinbox_entry: str.isdigit(spinbox_entry) or spinbox_entry==''
 
 
 
-#tk物件
-window = tk.Tk()
-window.geometry('200x100+600+300')
-spinbox = tk.Spinbox(from_=1, to=9, wrap='True', validate='key', validatecommand=(window.register(vcmd), '%P'))
-spinbox.grid(row=0, column=0, padx=20, pady=20)
+def main():
+    window = tk.Tk()
+    window.geometry('200x100+600+300')
+    spinbox = tk.Spinbox(from_=1, to=9, wrap='True', validate='key', validatecommand=(window.register(vcmd), '%P'))
+    spinbox.grid(row=0, column=0, padx=20, pady=20)
 
+    window.mainloop()
 
-
-window.mainloop()
+if __name__ == '__main__':
+    main()
